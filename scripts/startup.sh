45d92c56952a8d2a52d02729694e1c7a21f8cc4c
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2023-04-02 21:27:47 -0300

    ECS support

---
+++ scripts/startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -z ${FFKEY+x} ]; then 
+  echo "FF KEY is unset"; 
+else 
+  echo "FF KEY set to ${FFKEY}"; 
+  sed -i -r "s/[a-z0-9]*-[a-z0-9]*-[a-z0-9]*-[a-z0-9]*-[a-z0-9]*/${FFKEY}/g" /usr/share/nginx/html/js/ff.js; 
+fi;
+
+cat /opt/scripts/nginx.conf >> /etc/nginx/nginx.conf.tmp
+mv /etc/nginx/nginx.conf.tmp /etc/nginx/nginx.conf
+
+nginx -c /etc/nginx/nginx.conf -g "daemon off;"
